0608d263620df7a702d1ea023d8f6e5056e8d107
Christian Mayer <christian@fox21.at>
2015-02-27 23:42:57 +0100

    i386 & x86_64 nop added.

---
+++ src/TheFox/Assembly/Instruction/I386/Nop.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheFox\Assembly\Instruction\I386;
+
+use TheFox\Assembly\Instruction\X86\Nop as X86Nop;
+
+class Nop extends Instruction{
+	
+}
---
+++ src/TheFox/Assembly/Instruction/X86_64/Nop.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheFox\Assembly\Instruction\X86_64;
+
+use TheFox\Assembly\Instruction\I386\Nop as I386Nop;
+
+class Nop extends Instruction{
+	
+}
