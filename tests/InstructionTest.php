9db9f8f162fd38a32394d028db1bb12973518b16
Christian Mayer <christian@fox21.at>
2015-02-27 23:46:50 +0100

    Tests.

--- tests/InstructionTest.php
+++ tests/InstructionTest.php
@@ -7,6 +7,7 @@ class InstructionTest extends PHPUnit_Framework_TestCase{
 	public function testSetOpcode(){
 		$instr = new Instruction();
 		$instr->setOpcode('90');
+		
 		$this->assertEquals('90', $instr->getOpcode());
 		$this->assertEquals('90', $instr->assemble());
 	}
