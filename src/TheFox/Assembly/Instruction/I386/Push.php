0448ca1376c9642c8e40a6aef7334f88874437ce
Christian Mayer <christian@fox21.at>
2015-02-27 23:23:49 +0100

    Clean up.

--- src/TheFox/Assembly/Instruction/I386/Push.php
+++ src/TheFox/Assembly/Instruction/I386/Push.php
@@ -8,7 +8,6 @@ class Push extends Instruction{
 	
 	public function __construct($register){
 		$register = strtolower($register);
-		#$this->setOpcode(pack('H*', 'C3'));
 		
 		$preC = pack('H*', '66');
 		$pre = '';
@@ -17,29 +16,29 @@ class Push extends Instruction{
 			case 'ax':
 				$pre = $preC;
 			case 'eax':
-				$push = new X86Push('ax');
-				$this->setOpcode($pre.$push->assemble());
+				$instr = new X86Push('ax');
+				$this->setOpcode($pre.$instr->assemble());
 				break;
 			
 			case 'cx':
 				$pre = $preC;
 			case 'ecx':
-				$push = new X86Push('cx');
-				$this->setOpcode($pre.$push->assemble());
+				$instr = new X86Push('cx');
+				$this->setOpcode($pre.$instr->assemble());
 				break;
 			
 			case 'dx':
 				$pre = $preC;
 			case 'edx':
-				$push = new X86Push('dx');
-				$this->setOpcode($pre.$push->assemble());
+				$instr = new X86Push('dx');
+				$this->setOpcode($pre.$instr->assemble());
 				break;
 			
 			case 'bx':
 				$pre = $preC;
 			case 'ebx':
-				$push = new X86Push('bx');
-				$this->setOpcode($pre.$push->assemble());
+				$instr = new X86Push('bx');
+				$this->setOpcode($pre.$instr->assemble());
 				break;
 		}
 	}
