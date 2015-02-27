7d888bd6907cbee5fb8728742ed2d858244e6b79
Christian Mayer <christian@fox21.at>
2015-02-28 00:15:43 +0100

    Tests.

--- tests/AssemblyTest.php
+++ tests/AssemblyTest.php
@@ -1,8 +1,10 @@
 <?php
 
 use TheFox\Assembly\Assembly;
-use TheFox\Assembly\Instruction\X86\Nop;
-use TheFox\Assembly\Instruction\X86\Ret;
+use TheFox\Assembly\Instruction\X86\Nop as X86Nop;
+use TheFox\Assembly\Instruction\X86\Ret as X86Ret;
+use TheFox\Assembly\Instruction\I386\Nop as I386Nop;
+use TheFox\Assembly\Instruction\I386\Ret as I386Ret;
 
 class AssemblyTest extends PHPUnit_Framework_TestCase{
 	
@@ -10,10 +12,21 @@ class AssemblyTest extends PHPUnit_Framework_TestCase{
 		$this->assertEquals('Assembly', Assembly::NAME);
 	}
 	
-	public function testAssemble(){
+	public function testAssembleX86(){
 		$asm = new Assembly();
-		$asm->addInstruction(new Nop());
-		$asm->addInstruction(new Ret());
+		$asm->addInstruction(new X86Nop());
+		$asm->addInstruction(new X86Ret());
+		
+		$opcode = unpack('H*', $asm->assemble());
+		$opcode = $opcode[1];
+		
+		$this->assertEquals('90c3', $opcode);
+	}
+	
+	public function testAssembleI386(){
+		$asm = new Assembly();
+		$asm->addInstruction(new I386Nop());
+		$asm->addInstruction(new I386Ret());
 		
 		$opcode = unpack('H*', $asm->assemble());
 		$opcode = $opcode[1];
