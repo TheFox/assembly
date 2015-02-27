463c9c5aff3596b36afce52ab00309878d23d006
Christian Mayer <christian@fox21.at>
2015-02-27 23:42:24 +0100

    i386 & x86_64 ret added.

---
+++ src/TheFox/Assembly/Instruction/I386/Ret.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheFox\Assembly\Instruction\I386;
+
+use TheFox\Assembly\Instruction\X86\Ret as X86Ret;
+
+class Ret extends Instruction{
+	
+}
---
+++ src/TheFox/Assembly/Instruction/X86_64/Ret.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheFox\Assembly\Instruction\X86_64;
+
+use TheFox\Assembly\Instruction\I386\Ret as I386Ret;
+
+class Ret extends Instruction{
+	
+}
