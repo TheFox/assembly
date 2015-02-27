9a5eb079653681558ec2533267a023861d338dc5
Christian Mayer <christian@fox21.at>
2015-02-27 23:50:59 +0100

    Code style fix.

--- src/TheFox/Assembly/Instruction/X86/Pop.php
+++ src/TheFox/Assembly/Instruction/X86/Pop.php
@@ -10,10 +10,18 @@ class Pop extends Instruction{
 		$register = strtolower($register);
 		#$this->setOpcode(pack('H*', 'C3'));
 		switch($register){
-			case 'ax': $this->setOpcode(pack('H*', '58')); break;
-			case 'cx': $this->setOpcode(pack('H*', '59')); break;
-			case 'dx': $this->setOpcode(pack('H*', '5A')); break;
-			case 'bx': $this->setOpcode(pack('H*', '5B')); break;
+			case 'ax':
+				$this->setOpcode(pack('H*', '58'));
+				break;
+			case 'cx':
+				$this->setOpcode(pack('H*', '59'));
+				break;
+			case 'dx':
+				$this->setOpcode(pack('H*', '5A'));
+				break;
+			case 'bx':
+				$this->setOpcode(pack('H*', '5B'));
+				break;
 		}
 	}
 	
--- src/TheFox/Assembly/Instruction/X86/Push.php
+++ src/TheFox/Assembly/Instruction/X86/Push.php
@@ -10,10 +10,18 @@ class Push extends Instruction{
 		$register = strtolower($register);
 		#$this->setOpcode(pack('H*', 'C3'));
 		switch($register){
-			case 'ax': $this->setOpcode(pack('H*', '50')); break;
-			case 'cx': $this->setOpcode(pack('H*', '51')); break;
-			case 'dx': $this->setOpcode(pack('H*', '52')); break;
-			case 'bx': $this->setOpcode(pack('H*', '53')); break;
+			case 'ax':
+				$this->setOpcode(pack('H*', '50'));
+				break;
+			case 'cx':
+				$this->setOpcode(pack('H*', '51'));
+				break;
+			case 'dx':
+				$this->setOpcode(pack('H*', '52'));
+				break;
+			case 'bx':
+				$this->setOpcode(pack('H*', '53'));
+				break;
 		}
 	}
 	
