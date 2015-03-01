f51f2205ada38e287c2d7c153d78eaf6893ff134
Christian Mayer <christian@fox21.at>
2015-03-01 21:56:27 +0100

    x86_64 tests added.

--- tests/NopTest.php
+++ tests/NopTest.php
@@ -2,6 +2,7 @@
 
 use TheFox\Assembly\Instruction\X86\Nop as X86Nop;
 use TheFox\Assembly\Instruction\I386\Nop as I386Nop;
+use TheFox\Assembly\Instruction\X86_64\Nop as X8664Nop;
 
 class NopTest extends PHPUnit_Framework_TestCase{
 	
@@ -14,6 +15,13 @@ class NopTest extends PHPUnit_Framework_TestCase{
 	
 	public function testI386(){
 		$instr = new I386Nop();
+		$opcode = unpack('H*', $instr->assemble());
+		$opcode = $opcode[1];
+		$this->assertEquals('90', $opcode);
+	}
+	
+	public function testX8664(){
+		$instr = new X8664Nop();
 		$opcode = unpack('H*', $instr->assemble());
 		$opcode = $opcode[1];
 		$this->assertEquals('90', $opcode);
--- tests/RetTest.php
+++ tests/RetTest.php
@@ -2,6 +2,7 @@
 
 use TheFox\Assembly\Instruction\X86\Ret as X86Ret;
 use TheFox\Assembly\Instruction\I386\Ret as I386Ret;
+use TheFox\Assembly\Instruction\X86_64\Ret as X8664Ret;
 
 class RetTest extends PHPUnit_Framework_TestCase{
 	
@@ -14,6 +15,13 @@ class RetTest extends PHPUnit_Framework_TestCase{
 	
 	public function testI386(){
 		$instr = new I386Ret();
+		$opcode = unpack('H*', $instr->assemble());
+		$opcode = $opcode[1];
+		$this->assertEquals('c3', $opcode);
+	}
+	
+	public function testX8664(){
+		$instr = new X8664Ret();
 		$opcode = unpack('H*', $instr->assemble());
 		$opcode = $opcode[1];
 		$this->assertEquals('c3', $opcode);
