04b97b01fc2d747a4d75b208a413c3c9e636bd46
Christian Mayer <christian@fox21.at>
2015-03-01 21:52:51 +0100

    i386 tests added.

--- tests/NopTest.php
+++ tests/NopTest.php
@@ -1,11 +1,19 @@
 <?php
 
 use TheFox\Assembly\Instruction\X86\Nop as X86Nop;
+use TheFox\Assembly\Instruction\I386\Nop as I386Nop;
 
 class NopTest extends PHPUnit_Framework_TestCase{
 	
 	public function testX86(){
 		$instr = new X86Nop();
+		$opcode = unpack('H*', $instr->assemble());
+		$opcode = $opcode[1];
+		$this->assertEquals('90', $opcode);
+	}
+	
+	public function testI386(){
+		$instr = new I386Nop();
 		$opcode = unpack('H*', $instr->assemble());
 		$opcode = $opcode[1];
 		$this->assertEquals('90', $opcode);
--- tests/RetTest.php
+++ tests/RetTest.php
@@ -1,11 +1,19 @@
 <?php
 
 use TheFox\Assembly\Instruction\X86\Ret as X86Ret;
+use TheFox\Assembly\Instruction\I386\Ret as I386Ret;
 
 class RetTest extends PHPUnit_Framework_TestCase{
 	
 	public function testX86(){
 		$instr = new X86Ret();
+		$opcode = unpack('H*', $instr->assemble());
+		$opcode = $opcode[1];
+		$this->assertEquals('c3', $opcode);
+	}
+	
+	public function testI386(){
+		$instr = new I386Ret();
 		$opcode = unpack('H*', $instr->assemble());
 		$opcode = $opcode[1];
 		$this->assertEquals('c3', $opcode);
