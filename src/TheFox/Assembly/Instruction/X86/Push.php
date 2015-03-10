b7be775a99aee8aedb445e776b25cdd2b4971a5d
Christian Mayer <christian@fox21.at>
2015-03-10 19:56:42 +0100

    Clean up.

--- src/TheFox/Assembly/Instruction/X86/Push.php
+++ src/TheFox/Assembly/Instruction/X86/Push.php
@@ -7,26 +7,6 @@ namespace TheFox\Assembly\Instruction\X86;
 class Push extends Instruction{
 	
 	public function __construct($register){
-		/*$register = strtolower($register);
-		$opcode = '';
-		switch($register){
-			case 'ax':
-				$opcode = '50';
-				break;
-			case 'cx':
-				$opcode = '51';
-				break;
-			case 'dx':
-				$opcode = '52';
-				break;
-			case 'bx':
-				$opcode = '53';
-				break;
-		}
-		if($opcode){
-			$this->setOpcode(pack('H*', $opcode));
-		}*/
-		
 		if($register){
 			$register = strtolower($register);
 			if($register[1] == 'x'){
