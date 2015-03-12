d28ccbf6575e16ee187bc97719e03de5a33fe9b6
Christian Mayer <christian@fox21.at>
2015-03-12 22:44:13 +0100

    Example.

--- example.php
+++ example.php
@@ -4,15 +4,22 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use TheFox\Assembly\Assembly;
 use TheFox\Assembly\Instruction\X86_64\Mov;
+use TheFox\Assembly\Instruction\X86_64\Jmp;
 use TheFox\Assembly\Instruction\X86_64\Nop;
 use TheFox\Assembly\Instruction\X86_64\Ret;
 
+
+$retInstr = new Ret();
+
 $asm = new Assembly();
 $asm->addInstruction(new Mov('rax', 'rbx'));
+$asm->addInstruction(new Jmp($retInstr));
 $asm->addInstruction(new Nop());
-$asm->addInstruction(new Ret());
+$asm->addInstruction($retInstr);
+$asm->addInstruction(new Nop());
 
 $opcode = $asm->assemble();
 
 $hex = unpack('H*', $opcode);
-\Doctrine\Common\Util\Debug::dump($hex);
+$hex = $hex[1];
+print 'hex opcode: '.$hex."\n";
